d1d5fcb2a508cf1bbae642237d1701668a4e7b40
Gabor Csardi <csardi.gabor@gmail.com>
2014-10-30 12:27:32 -0400

    Add DB update update shell script
    To be run daily from cron.

---
+++ db/update.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+# RStudio base URL
+url='http://cran-logs.rstudio.com/2014/<date>.csv.gz'
+
+# Last day in the DB
+max_day=$(echo 'SELECT MAX(day) FROM daily;' |
+          dokku psql:restore_sql cranlogs |
+	  grep '[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]')
+
+# Start here
+day=$(date --date "$max_day 1 day" +%Y-%m-%d)
+
+# First day *not* on the RStudio server is probably today
+today=$(date --date today '+%Y-%m-%d')
+
+# Function to add one day
+function do_day() {
+    day=$1
+    filename=$day.csv.gz
+    trap "rm -f $filename" EXIT
+    
+    echo -n "Downloading $day,"
+    curl -s -O $(echo $url | sed "s/<date>/$day/")
+
+    echo -n " parsing"
+    filename2=$day.sql
+    trap "rm -f $filename2" EXIT
+    echo 'COPY daily (day, package, count) FROM stdin;' > $filename2
+    zcat $filename       |
+	cut -d, -f1,7    |	# take day and package name
+	tr -d '"'        |	# remove quotes
+	tr , '\t'        |	# replace commas with tabs
+	tail -n +2       |	# skip header line
+	sort             |	# sort
+	uniq -c          |	# count number of downloads per day
+	grep -v '\sNA\s' | 	# remove NA package names
+	awk 'BEGIN { OFS="\t"; } { print $2, $3, $1; }' >> $filename2
+    echo '\.' >>$filename2
+
+    echo -n " adding"
+    dokku psql:restore_sql cranlogs < $filename2
+
+    echo -n " cleaning"
+    rm -f $filename $filename2
+    echo " DONE."
+}
+
+# Iterate over all days 
+until [ "$day" == "$today" ]
+do
+    do_day $day
+    day=$(/bin/date --date "$day 1 day" +%Y-%m-%d)
+done
