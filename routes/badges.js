8833c943d3c4b158422941f9b13be091e8ded291
Gabor Csardi <csardi.gabor@gmail.com>
2015-04-23 16:22:19 -0400

    Badges: per month or week or day, #9

--- routes/badges.js
+++ routes/badges.js
@@ -5,20 +5,27 @@ var multiline = require('multiline');
 
 var conString = process.env.DATABASE_URL;
 
+var svg_params = {
+    "grand-total": { "width": 108, "textwidth": 88,    "path_d": 38 },
+    "last-month":  { "width": 149, "textwidth": 108.5, "path_d": 79 },
+    "last-week":   { "width": 143, "textwidth": 105.5, "path_d": 73 },
+    "last-day":    { "width": 134, "textwidth": 101,   "path_d": 64 }
+};
+
 var badge_svg = multiline(function(){/*
-<svg xmlns="http://www.w3.org/2000/svg" width="108" height="20">
+<svg xmlns="http://www.w3.org/2000/svg" width=":width:" height="20">
   <linearGradient id="b" x2="0" y2="100%">
     <stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
     <stop offset="1" stop-opacity=".1"/>
   </linearGradient>
   <mask id="a">
-    <rect width="108" height="20" rx="3" fill="#fff"/>
+    <rect width=":width:" height="20" rx="3" fill="#fff"/>
   </mask>
   <g mask="url(#a)"><path fill="#555" d="M0 0h70v20H0z"/>
-    <path fill="#007ec6" d="M70 0h38v20H70z"/>
-    <path fill="url(#b)" d="M0 0h108v20H0z"/>
+    <path fill="#007ec6" d="M70 0h:path_d:v20H70z"/>
+    <path fill="url(#b)" d="M0 0h:width:v20H0z"/>
   </g>
-  <g fill="#fff" text-anchor="middle" 
+  <g fill="#fff" text-anchor="middle"
      font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">
     <text x="36" y="15" fill="#010101" fill-opacity=".3">
       downloads
@@ -26,30 +33,43 @@ var badge_svg = multiline(function(){/*
     <text x="36" y="14">
       downloads
     </text>
-    <text x="88" y="15" fill="#010101" fill-opacity=".3">
-      :XXX:
+    <text x=":textwidth:" y="15" fill="#010101" fill-opacity=".3">
+      :count:
     </text>
-    <text x="88" y="14">
-      :XXX:
+    <text x=":textwidth:" y="14">
+      :count:
     </text>
   </g>
 </svg>
 */});
 
-var re_pre = '^\\/';
-var re_pkg = '([\\w\\.,]+)';
+var re_pre = '^';
+var re_slash = '\\/';
+var re_key = '(last-day|last-week|last-month|grand-total)';
+var re_pkg = '(?:\\/([\\w\\.,]+))?';
 var re_suf = '$';
-var re_full = new RegExp(re_pre + re_pkg + re_suf, 'i');
+var re_full = new RegExp(re_pre + re_slash + re_key + re_pkg + re_suf, 'i');
+
+var re_old = new RegExp(re_pre + re_pkg + re_suf, 'i');
 
 router.get(re_full, function(req, res) {
+    var interval = req.params[0];
+    var package = req.params[1] || req.params[0];
+    do_query(res, package, interval);
+});
+
+router.get(re_old, function(req, res) {
     var package = req.params[0];
+    var interval = 'last-month';
+    do_query(res, package, interval);
+});
+
+function do_query(res, package, interval) {
+
     var now = new Date().toUTCString();
     res.set('Content-Type', 'image/svg+xml');
     res.set('Expires', now);
-    do_query(res, package);
-});
 
-function do_query(res, package) {
     pg.connect(conString, function(err, client, done) {
 
 	if (err) {
@@ -60,31 +80,87 @@ function do_query(res, package) {
 	    return true;
 	}
 
-	var q = 'SELECT SUM(count) FROM DAILY WHERE package = \'' +
-	    package + '\'';
-	client.query(q, function(err, result) {
-	    if (err) {
-		done();
-		res.set(500);
-		res.end('{ "error": Cannot query DB", ' +
-			'  "email": "csardi.gabor+cranlogs@gmail.com" }');
-		return true;
-	    }
-
-	    var sum = result['rows'][0]['sum'];
-	    if (sum >= 1000000) {
-		sum = Math.round(sum / 100000) / 10 + 'M';
-	    } else if (sum >= 10000) {
-		sum = Math.round(sum / 1000) + 'K';
-	    }
-	    var svg = badge_svg.replace(/:XXX:/g, sum);
-
-	    done();
-	    res.set(200);
-	    res.send(svg);
-	    res.end();
-	});
+	if (interval == "grand-total") {
+	    do_total(res, client, package, done)
+	} else {
+	    do_interval(res, client, package, interval, done)
+	}
     });
+}
+
+function do_total(res, client, package, done) {
+
+    var q = 'SELECT SUM(count) FROM DAILY WHERE package = \'' +
+	package + '\'';
+    client.query(q, function(err, result) {
+	if (err) {
+	    done();
+	    res.set(500);
+	    res.end('{ "error": Cannot query DB", ' +
+		    '  "email": "csardi.gabor+cranlogs@gmail.com" }');
+	    return true;
+	}
+
+	var sum = pretty_count(result['rows'][0]['sum']);
+	var params = svg_params['grand-total'];
+	params['count'] = sum;
+	var svg = svg_template(badge_svg, params);
+
+	done();
+
+	res.set(200);
+	res.send(svg);
+	res.end();
+    });
+}
+
+function do_interval(res, client, package, interval, done) {
+
+    var q = 'SELECT cl_total_json(\'' + interval + '\', \'' +
+	package + '\')';
+    console.log(q);
+
+    client.query(q, function(err, result) {
+	if (err) {
+	    done();
+	    res.set(500);
+	    res.end('{ "error": Cannot query DB", ' +
+		    '  "email": "csardi.gabor+cranlogs@gmail.com" }');
+	    return true;
+	}
+
+	var sum = pretty_count(result['rows'][0]['cl_total_json']['downloads']);
+	sum = sum + "/" + interval.replace('last-', '');
+	var params = svg_params[interval]
+	params['count'] = sum;
+	var svg = svg_template(badge_svg, params);
+
+	done();
+
+	res.set(200);
+	res.send(svg);
+	res.end();
+
+    });
+}
+
+function svg_template(svg, params) {
+    for (var key in params) {
+	if (params.hasOwnProperty(key)) {
+	    var regexp = new RegExp(':' + key + ":", 'g')
+	    svg = svg.replace(regexp, params[key])
+	}
+    }
+    return svg;
+}
+
+function pretty_count(sum) {
+    if (sum >= 1000000) {
+	sum = Math.round(sum / 100000) / 10 + 'M';
+    } else if (sum >= 10000) {
+	sum = Math.round(sum / 1000) + 'K';
+    }
+    return sum
 }
 
 module.exports = router;
